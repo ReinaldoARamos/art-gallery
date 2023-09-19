468568c8dc91153c6e0006e53ab531fad72e83b5
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-09-19 11:16:20 -0300

    finalziando base do header e colocando responsidivdae

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,9 +2,9 @@ import Logo from "./Logo";
 
 export default function Header() {
   return (
-    <div className="flex w-full items-center justify-between bg-base px-72 text-zinc-200  ">
-      <Logo />
-      <div className="group flex gap-8">
+    <div className="flex  w-full items-center justify-between bg-base md:px-72 text-zinc-200  h-28 ">
+      <Logo className="md:flex hidden"/>
+      <div className="group flex gap-8 md:flex sm:flex lg:w-80 sm:items-center sm:w-full md:w-full sm:justify-center md:justify-center ">
         <a href="" className="cursor-pointer transition  hover:text-white">
           Início
         </a>
