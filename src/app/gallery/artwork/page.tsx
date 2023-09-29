896b137342cee4770b96449934a97ac95f08bd78
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-09-29 11:29:14 -0300

    criando o banner das páginas

--- src/app/gallery/artwork/page.tsx
+++ src/app/gallery/artwork/page.tsx
@@ -5,102 +5,106 @@ import { Directory } from '@/app/components/Directory/Directory'
 import { NextPageButton } from '@/app/components/NextPageButton/NextPageButton'
 import { SearchBar } from '@/app/components/Searchbar/SearchBar'
 import { ArrowLeftCircle, ArrowRightCircle } from 'lucide-react'
+import Image from 'next/image'
 
 export default function Artists() {
   return (
-    <div className="px-72">
-      <div className="pt-7 pb-14">
-        <Directory />
-        <SearchBar />
+    <>
+      <div className="bg-[url('/images/artwork-banner.jpg')] bg-cover h-64 "></div>
+      <div className="px-72">
+        <div className="pt-7 pb-14">
+          <Directory />
+          <SearchBar />
+        </div>
+        <h1 className="text-title text-4xl pb-9">Todos os artistas</h1>
+        <div className="grid grid-cols-4 gap-6">
+          <Board
+            author="Diego Pires"
+            height={315}
+            width={315}
+            name="Foco Inevitável"
+            src="/images\obras\image.png"
+          />
+          <Board
+            author="Paula"
+            height={315}
+            width={315}
+            name="Fragmentos"
+            src="/images\obras\image-1.png"
+          />
+          <Board
+            author="Diego Pires"
+            height={315}
+            width={315}
+            name="Dançando com as Cores"
+            src="/images\obras\image-2.png"
+          />
+          <Board
+            author="Ana Maria"
+            height={315}
+            width={315}
+            name="Expressão Profunda"
+            src="/images\obras\image-3.png"
+          />
+          <Board
+            author="Amanda Brito"
+            height={315}
+            width={315}
+            name="Vigilância Multifocal"
+            src="/images\obras\image-4.png"
+          />
+          <Board
+            author="Gustavo Alves"
+            height={315}
+            width={315}
+            name="Melodia Cromática"
+            src="/images\obras\image-5.png"
+          />
+          <Board
+            author="Diego Pires"
+            height={315}
+            width={315}
+            name="Escorrendo em Cores"
+            src="/images\obras\image-6.png"
+          />
+          <Board
+            author="Paula"
+            height={315}
+            width={315}
+            name="O Contraste"
+            src="/images\obras\image-7.png"
+          />
+          <Board
+            author="Ana Maria"
+            height={315}
+            width={315}
+            name="Espírito Ancestral"
+            src="/images\obras\image-8.png"
+          />
+          <Board
+            author="Amanda Brito"
+            height={315}
+            width={315}
+            name="Melancolia Velada"
+            src="/images\obras\image-9.png"
+          />
+          <Board
+            author="Artur Nogueira"
+            height={315}
+            width={315}
+            name="Laços Genéticos"
+            src="/images\obras\image-10.png"
+          />
+          <Board
+            author="Gustavo Alves"
+            height={315}
+            width={315}
+            name="Inocência em Harmonia"
+            src="/images\obras\image-11.png"
+          />
+        </div>
+        <NextPageButton />
       </div>
-      <h1 className="text-title text-4xl pb-9">Todos os artistas</h1>
-      <div className="grid grid-cols-4 gap-6">
-        <Board
-          author="Diego Pires"
-          height={315}
-          width={315}
-          name="Foco Inevitável"
-          src="/images\obras\image.png"
-        />
-        <Board
-          author="Paula"
-          height={315}
-          width={315}
-          name="Fragmentos"
-          src="/images\obras\image-1.png"
-        />
-        <Board
-          author="Diego Pires"
-          height={315}
-          width={315}
-          name="Dançando com as Cores"
-          src="/images\obras\image-2.png"
-        />
-        <Board
-          author="Ana Maria"
-          height={315}
-          width={315}
-          name="Expressão Profunda"
-          src="/images\obras\image-3.png"
-        />
-        <Board
-          author="Amanda Brito"
-          height={315}
-          width={315}
-          name="Vigilância Multifocal"
-          src="/images\obras\image-4.png"
-        />
-        <Board
-          author="Gustavo Alves"
-          height={315}
-          width={315}
-          name="Melodia Cromática"
-          src="/images\obras\image-5.png"
-        />
-        <Board
-          author="Diego Pires"
-          height={315}
-          width={315}
-          name="Escorrendo em Cores"
-          src="/images\obras\image-6.png"
-        />
-        <Board
-          author="Paula"
-          height={315}
-          width={315}
-          name="O Contraste"
-          src="/images\obras\image-7.png"
-        />
-        <Board
-          author="Ana Maria"
-          height={315}
-          width={315}
-          name="Espírito Ancestral"
-          src="/images\obras\image-8.png"
-        />
-        <Board
-          author="Amanda Brito"
-          height={315}
-          width={315}
-          name="Melancolia Velada"
-          src="/images\obras\image-9.png"
-        />
-        <Board
-          author="Artur Nogueira"
-          height={315}
-          width={315}
-          name="Laços Genéticos"
-          src="/images\obras\image-10.png"
-        />
-        <Board
-          author="Gustavo Alves"
-          height={315}
-          width={315}
-          name="Inocência em Harmonia"
-          src="/images\obras\image-11.png"
-        />
-      </div>
-      <NextPageButton />
-    </div>
+    </>
   )
 }
--- src/app/gallery/interview/page.tsx
+++ src/app/gallery/interview/page.tsx
@@ -3,26 +3,29 @@ import { Directory } from '@/app/components/Directory/Directory'
 import { InterviewCard } from '@/app/components/InterviewCard/InterviewCard'
 import { NextPageButton } from '@/app/components/NextPageButton/NextPageButton'
 import { SearchBar } from '@/app/components/Searchbar/SearchBar'
-
+import Image from 'next/image'
 export default function Intervirw() {
   return (
-    <div className="px-72 pt-7 ">
-      <Directory />
-      <SearchBar />
-      <div className="pt-14">
-        <h1 className="text-title font-bold text-4xl pb-8">
-          Todas as entrevistas
-        </h1>
-        <div className="grid grid-cols-2 pb-24">
-          <InterviewCard />
-          <InterviewCard />
-          <InterviewCard />
-          <InterviewCard />
-          <InterviewCard />
-          <InterviewCard />
+    <>
+      <div className="bg-[url('/images/inteview-banner.png')] bg-cover h-64 "></div>
+      <div className="px-72 pt-7 ">
+        <Directory />
+        <SearchBar />
+        <div className="pt-14">
+          <h1 className="text-title font-bold text-4xl pb-8">
+            Todas as entrevistas
+          </h1>
+          <div className="grid grid-cols-2 pb-24">
+            <InterviewCard />
+            <InterviewCard />
+            <InterviewCard />
+            <InterviewCard />
+            <InterviewCard />
+            <InterviewCard />
+          </div>
+          <NextPageButton />
         </div>
-        <NextPageButton />
       </div>
-    </div>
+    </>
   )
 }
