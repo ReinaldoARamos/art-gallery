13e0a4a21e54253f383a8570078260cd2eddf788
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-10-05 17:41:48 -0300

    criando a página de artigos e tutoriais

--- src/app/blog/tutorials/page.tsx
+++ src/app/blog/tutorials/page.tsx
@@ -12,45 +12,36 @@ export default function Articles() {
     <>
       <div className="bg-[url('/images/artist-banner.png')] bg-cover h-64 " />
       <div className="px-72 pt-7">
-        <Directory path1="Blog" path2="Artigos" />
+        <Directory path1="Blog" path2="Tutoriais" />
 
-        <h1 className="text-title text-4xl pt-14 pb-16 font-bold">Artigos</h1>
+        <h1 className="text-title text-4xl pt-14 pb-16 font-bold">Tutoriais</h1>
         <div className="flex gap-5">
           <div className="space-y-12">
             <ArticlesPost
-              src={'/images/post-1.png'}
+              src={'/images/post-3.png'}
               author={'Júlia Albuquerque'}
-              title={'10 artistas que voce deveria seguir'}
+              title={'A Arte Viva das Ruas'}
               date={'30/05/2023'}
               article={
                 'De norte a sul e de leste a oeste, uma coisa que você não deixará de descobrir em todo o Brasil em quase todas as ruas, há tags, desenhos, graffitis e pinturas nas paredes. O Brasil é conhecido mundialmente pela sua street art. Nas ruas brasileiras podemos encontrar tags, desenhos, grafites, pinturas de parede, lambe-lambe, stickers, é uma infinidade de intervenções urbanas provocadas por artistas das mais variadas técnicas.'
               }
             />
             <ArticlesPost
-              src={'/images/post-2.png'}
+              src={'/images/post-4.png'}
               author={'Júlia Albuquerque'}
-              title={'O que é arte Urbana?'}
+              title={'Expressando-se na Cidade'}
               date={'30/05/2023'}
               article={
                 'É um tipo de arte encontrada nos espaços urbanos. Manifesta-se por meio de intervenções, performances, grafite, teatro, dentre outras. Essas ações artísticas ocorrem em ambientes públicos e interagem diretamente com os indivíduos. Geralmente, usam como suporte os grandes centros urbanos, onde há intensa circulação de pessoas e diversidade cultural. Dessa forma, os cidadãos acabam se deparando com a arte sem precisar ir até centros culturais.'
               }
             />
-            <ArticlesPost
-              src={'/images/post-5.png'}
-              author={'Júlia Albuquerque'}
-              title={'9 museus especializados em gravuras'}
-              date={'30/05/2023'}
-              article={
-                'Museu é o local ideal para quem deseja viajar nas férias de forma cultural. Para sair do convencional nada melhor que visitar museus especializados em gravuras ou estampas. Nós montamos um pequeno circuito dedicado a arte da impressão gráfica. Podemos começar por um museu bem acessível, um pouco longe para muitos, mas bem mais perto do que os outros da lista.'
-              }
-            />
           </div>
           <div className="space-y-4">
             <div className="space-y-1">
-              <span>Encontre um artigo</span>
+              <span>Encontre um tutorial</span>
               <div className="flex gap-5">
                 <input
-                  placeholder="Nome do artigo"
+                  placeholder="Nome do tutorial"
                   className="border-gray300 border rounded-lg focus:outline-none p-3"
                 />
                 <div>
--- src/app/components/ArticlesPost/ArticlesPost.tsx
+++ src/app/components/ArticlesPost/ArticlesPost.tsx
@@ -19,18 +19,20 @@ export function ArticlesPost({
   title,
 }: ArticlePostProps) {
   return (
-    <div className="flex gap-5">
+    <div className="flex gap-5  ">
       <Image alt="" src={src} width={312} height={365} />
-      <div className="flex flex-col">
-        <h1 className="text-title font-bold text-3xl pb-3">{title}</h1>
-        <div className="flex flex-col space-y-1 pb-9">
-          <span className="text-boardbrown text-md">
-            Publicado por {author}
-          </span>
-          <span className="text-brown text-sm">Em {date}</span>
-        </div>
-        <div className="pb-9  w-[33.75rem] text-justify text-boardbrown text-sm">
-          {article}
+      <div className="flex flex-col justify-between">
+        <div>
+          <h1 className="text-title font-bold text-3xl pb-3">{title}</h1>
+          <div className="flex flex-col space-y-1 pb-9">
+            <span className="text-boardbrown text-md">
+              Publicado por {author}
+            </span>
+            <span className="text-brown text-sm">Em {date}</span>
+          </div>
+          <div className="  w-[33.75rem] text-justify text-boardbrown text-sm">
+            {article}
+          </div>
         </div>
         <div>
           <Button variant="lightwithborder">
