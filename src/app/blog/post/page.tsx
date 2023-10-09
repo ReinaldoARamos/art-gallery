2d4bc57ccfcecf92553762414bd42533d974899d
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-10-08 22:37:05 -0300

    criando a tela do tutorial e posts

---
+++ src/app/blog/post/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { Directory } from '@/app/components/Directory/Directory'
+
+export default function Post() {
+  return (
+    <>
+      <div className="bg-[url('/images/tutorial-banner.png')] bg-cover h-64 " />
+      <div className="px-72">
+        <div className="py-7 pb-36">
+          <Directory path1="Blog" path2="O que é arte urbana" />
+        </div>
+        <h1 className="text-title font-bold text-3xl flex justify-center pb-24">
+          O que é arte urbana
+        </h1>
+        <div>
+          <div className="flex justify-center w-full  pb-16">
+            <div className="flex justify-center  text-justify  w-[62rem] ">
+              A arte de rua, muitas vezes vista como mero vandalismo, tem se
+              consolidado como uma forma legítima e valiosa de expressão
+              artística. Sendo utilizada tanto para embelezar as cidades como
+              para provocar reflexões sobre questões sociais, políticas e
+              culturais, a arte de rua tem se tornado um componente essencial do
+              tecido urbano moderno. O impacto da arte de rua é multifacetado.
+              Em primeiro lugar, ela transforma o ambiente urbano, conferindo
+              cor e vitalidade a locais antes cinzentos e sem vida. Ao utilizar
+              prédios, muros e calçadas como telas, os artistas de rua são
+              capazes de trazer beleza e criatividade a cada esquina, fazendo
+              com que os espaços públicos se tornem galerias de arte ao ar
+              livre. No entanto, a arte de rua não é apenas uma forma de
+              embelezamento urbano. Em muitos casos, ela é também uma poderosa
+              ferramenta de comentário social e político. Os artistas de rua
+              frequentemente utilizam suas obras para expressar opiniões,
+              questionar o status quo e gerar conscientização sobre questões
+              importantes, desde problemas locais até crises globais. Através do
+              seu caráter democrático e acessível, a arte de rua também desafia
+              a noção tradicional de que a arte é algo que pertence apenas a
+              museus e galerias, acessível apenas à elite. Ao contrário, a arte
+              de rua é acessível a todos, permitindo que pessoas de todas as
+              classes sociais tenham a oportunidade de apreciar, interagir e se
+              envolver com a arte em seu dia a dia. Além disso, a arte de rua
+              pode ter um impacto significativo na identidade e na cultura de
+              uma cidade. Ela reflete a diversidade, a energia e o espírito de
+              uma comunidade, ajudando a criar um senso de lugar e de
+              pertencimento. Cidades ao redor do mundo, de Berlim a Bogotá, de
+              Nova York a Melbourne, reconhecem o valor da arte de rua como uma
+              parte vital de sua identidade cultural. Em conclusão, a arte de
+              rua é mais do que uma simples expressão de criatividade
+              individual. Ela é uma forma de arte coletiva e colaborativa que
+              tem o poder de transformar cidades, inspirar comunidades e
+              provocar conversas. À medida que avançamos para o futuro, é
+              essencial que continuemos a valorizar e a apoiar a arte de rua
+              como uma parte essencial de nossa paisagem urbana.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/app/blog/postutorial/page.tsx
@@ -0,0 +1,93 @@
+'use client'
+
+import { Directory } from '@/app/components/Directory/Directory'
+
+export default function Post() {
+  return (
+    <>
+      <div className="bg-[url('/images/tutorial-banner.png')] bg-cover h-64 " />
+      <div className="px-72">
+        <div className="py-7 pb-36">
+          <Directory path1="Blog" path2="A Arte Viva das Ruas" />
+        </div>
+        <h1 className="text-title font-bold text-3xl flex justify-center pb-24">
+          A Arte Viva das Ruas
+        </h1>
+        <div>
+          <div className="flex justify-center w-full  pb-16">
+            <div className="flex justify-center  text-justify  w-[62rem] ">
+              Bem-vindo à seção de dicas e tutoriais! Meu nome é João Silva, um
+              artista de rua com mais de uma década de experiência em
+              transformar espaços urbanos em palcos para criatividade e
+              expressão. Hoje, estou aqui para compartilhar alguns insights
+              valiosos para aqueles que desejam explorar o mundo fascinante da
+              arte de rua. A arte de rua é um meio democrático, visceral e
+              incrivelmente expressivo. Ela permite que qualquer pessoa
+              transforme o mundo ao seu redor em uma tela, comunicando suas
+              ideias e sentimentos de forma vibrante e inescapável. No entanto,
+              também é uma forma de arte que exige prática, técnica e uma
+              compreensão do espaço público. 1. Planejamento e Design Antes de
+              pegar a lata de spray, planeje sua obra de arte. Esboce suas
+              ideias em um caderno e experimente diferentes designs e
+              composições. Considere o local onde você vai pintar. O que o
+              espaço pede? Como sua arte pode interagir com o ambiente? 2.
+              Entendendo o Spray Comprar tinta spray pode ser confuso para um
+              iniciante, mas não se preocupe. Nós vamos descomplicar. Existem
+              várias marcas, cada uma com suas características únicas.
+              Experimente algumas diferentes para descobrir qual você prefere.
+              3. Segurança em Primeiro Lugar Lembre-se de que a pintura a spray
+              pode ser perigosa se não manuseada corretamente. Use sempre uma
+              máscara de proteção ao pintar. Além disso, esteja ciente das leis
+              locais. A arte de rua pode ser ilegal em alguns lugares, então
+              procure por áreas onde é permitido pintar. 4. Praticando sua
+              Técnica Como qualquer outra habilidade, a prática leva à
+              perfeição. Comece praticando letras e formas básicas e, aos
+              poucos, avance para designs mais complexos. Lembre-se, todo
+              artista tem seu estilo único, então, não se preocupe em replicar o
+              trabalho de outra pessoa. Em vez disso, concentre-se em encontrar
+              a sua voz. 5. Respeitando o Espaço e a Comunidade Finalmente,
+              lembre-se de que a arte de rua é uma forma de comunicação com a
+              comunidade. Respeite o espaço dos outros e sempre procure maneiras
+              de contribuir positivamente para o local onde você está pintando.
+              A arte de rua é uma jornada, não um destino. Desfrute do processo,
+              aprenda com seus erros e continue se expressando. Bem-vindo ao
+              maravilhoso mundo da arte de rua! 6. Conheça a História da Arte de
+              Rua A arte de rua é profundamente enraizada na expressão cultural
+              e social. Conhecer as origens e a evolução da arte de rua pode não
+              só melhorar sua apreciação por esta forma de arte, mas também
+              fornecer uma fonte de inspiração para o seu próprio trabalho.
+              Desde as pichações políticas da Roma Antiga até o hip-hop dos anos
+              70 e a globalização da arte de rua através da internet, cada era
+              contribuiu de forma única para a tapeçaria da arte urbana. 7.
+              Domine o Uso de Stencils Os stencils são uma ferramenta poderosa
+              na arte de rua, permitindo a criação de imagens detalhadas e
+              repetitivas de forma rápida e eficiente. Domine esta técnica e
+              você será capaz de produzir obras de arte impressionantes com uma
+              consistência que seria difícil de alcançar apenas com tinta spray
+              livre. 8. Explore Técnicas Mistas A arte de rua não se limita
+              apenas à tinta spray. Considerando o uso de pôsteres, stickers,
+              instalações de arte ou até mesmo projeções digitais, você pode
+              trazer uma nova dimensão ao seu trabalho e se destacar dos demais.
+              Seja inovador e não tenha medo de experimentar. 9. Estabeleça sua
+              Identidade Artística Assim como a assinatura de um pintor, o seu
+              estilo e identidade são fundamentais na arte de rua. Quer você
+              escolha um pseudônimo, um logo ou um determinado estilo de
+              pintura, ter uma identidade consistente ajuda o público a
+              reconhecer e se conectar com o seu trabalho. 10. Se Conecte com
+              Outros Artistas de Rua Finalmente, uma das melhores maneiras de
+              crescer como artista é se envolver com a comunidade. Participe de
+              eventos de arte de rua, siga outros artistas nas redes sociais,
+              faça parte de workshops ou encontros. Esta é uma ótima maneira de
+              aprender novas técnicas, se inspirar e criar oportunidades de
+              colaboração. A arte de rua é sobre deixar sua marca no mundo,
+              comunicar sua mensagem e transformar o urbano em algo
+              extraordinário. Espero que estas dicas ajudem você a começar sua
+              jornada como artista de rua. Lembre-se, a rua é a sua tela. Feliz
+              pintura!
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
