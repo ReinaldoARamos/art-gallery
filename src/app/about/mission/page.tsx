fd75aef4afa1f036a2f515648c64bc7b97cd18ff
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-10-08 23:22:28 -0300

    Criando a base do about

---
+++ src/app/about/mission/page.tsx
@@ -0,0 +1,139 @@
+'use client'
+import { Board } from '@/app/components/Board/Board'
+import { Button } from '@/app/components/Button/ButtonTest'
+import { InterviewCard } from '@/app/components/InterviewCard/InterviewCard'
+import Testiominals from '@/app/components/Testiominals/Testiominals'
+import { UseRedirect } from '@/app/hooks/useRedirect'
+import { ArrowRightCircle } from 'lucide-react'
+
+export default function Gallery() {
+  const redirectTo = UseRedirect()
+
+  return (
+    <>
+      <div className="bg-[url('/images/our-mission.png')] bg-cover pt-32 pb-72 ">
+        <div className=" text-5xl bg-arrow  text-white font-bold flex justify-center items-center py-5">
+          <span>nossa missão</span>
+        </div>
+      </div>
+
+      <div className="pb-14 pt-8 font-bold text-title flex gap-3 justify-center items-center text-4xl  before:h-0.5 before:bg-gray before:w-96 after:h-0.5 after:bg-gray after:w-96 ">
+        Obras
+      </div>
+      <div className="px-72 flex flex-col">
+        <div>
+          <div>Para colecionar</div>
+          <div className="grid grid-cols-4 gap-5">
+            <Board
+              author="Ana Maria Silva"
+              height={315}
+              width={315}
+              name="Voo da liberdade"
+              src="/images\image.jpg"
+            />
+            <Board
+              author="Gustavo Alves"
+              height={315}
+              width={315}
+              name="Cidade em Movimento"
+              src="/images\image-1.jpg"
+            />
+            <Board
+              author="Diego Pires"
+              height={315}
+              width={315}
+              name="Dançando com as Cores"
+              src="/images\image-3.jpg"
+            />
+            <Board
+              author="Preiscilla Souza"
+              height={315}
+              width={315}
+              name="Melodia Urbana"
+              src="/images\image-2.jpg"
+            />
+            <Board
+              author="Burna Paiva"
+              height={315}
+              width={315}
+              name="O Beijo do Grafite"
+              src="/images\image-4.jpg"
+            />
+            <Board
+              author="Amanda Brito"
+              height={315}
+              width={315}
+              name="Sopro de Vida"
+              src="/images\image-5.jpg"
+            />
+            <Board
+              author="Denise Sobreira"
+              height={315}
+              width={315}
+              name="Esperança Urbana"
+              src="/images\image-7.jpg"
+            />
+            <Board
+              author="Eduardo Pinho"
+              height={315}
+              width={315}
+              name="Sonhos em Tinta"
+              src="/images\image-6.jpg"
+            />
+          </div>
+          <div className="flex justify-center">
+            <Button
+              variant="ghost"
+              onClick={() => redirectTo(`gallery/artwork`)}
+            >
+              Ver todas as obras
+              <ArrowRightCircle className="w-6 h-6" />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-5xl  font-bold text-Title pt-10 flex justify-center pb-10">
+        Depoimentos
+      </div>
+      <Testiominals
+        testimonials={[
+          {
+            author: 'Ana Maria Vieira',
+            depoiment:
+              'Como artista urbano, vejo nas ruas um quadro em branco onde posso expressar minhas emoções, pensamentos e crenças. Por muitos anos, lutei para encontrar um lugar onde meus pares e eu pudéssemos compartilhar nossas criações e experiências. Foi então que descobri o Projeto Arte Concreta. Desde o início, fui atraído pela abordagem única e inclusiva do site em relação à arte urbana. Aqui, todos os tipos de artistas são bem-vindos, independentemente do seu estilo ou nível de experiência. Aprendi muito com os tutoriais e dicas compartilhados no site, o que me ajudou a aprimorar minhas habilidades e expandir minha perspectiva artística.',
+          },
+          {
+            author: 'Gustavo Freitas',
+            depoiment:
+              'O Projeto Arte Concreta foi um ponto de virada na minha trajetória artística, proporcionando uma plataforma inclusiva e inspiradora. Através dele, encontrei um espaço para compartilhar minhas criações, aprender com outros artistas e expandir minha perspectiva. Essa comunidade acolhedora e diversa redefiniu minha abordagem à arte urbana, abrindo portas para novas possibilidades e desafios. Cada tutorial e interação moldou meu estilo e aprimorou minhas habilidades, tornando o Projeto Arte Concreta não apenas um site, mas uma fonte de crescimento artístico contínuo que mudou profundamente minha vida na arte.',
+          },
+          {
+            author: 'Arthur do Val',
+            depoiment:
+              'O Projeto Arte Concreta foi um divisor de águas em minha jornada artística, proporcionando uma plataforma inclusiva e inspiradora. Através dele, encontrei um espaço para compartilhar minhas criações, aprender com outros artistas e expandir minha perspectiva. Essa comunidade acolhedora e diversa redefiniu minha abordagem à arte urbana, abrindo portas para novas possibilidades e desafios. Cada tutorial e interação moldou meu estilo e aprimorou minhas habilidades, tornando o Projeto Arte Concreta não apenas um site, mas uma fonte de crescimento artístico contínuo que mudou profundamente minha vida na arte.',
+          },
+          // Add more testimonials as needed
+        ]}
+      />
+
+      <div className=" flex flex-col px-72 pb-20 ">
+        <div className="pt-16 flex justify-center items-center text-Title text-5xl pb-10">
+          Entrevistas
+        </div>
+        <div className="flex justify-center  item-center gap-5">
+          <InterviewCard src="/images/Video-2.jpg" />
+          <InterviewCard src="/images/Video-2.jpg" />
+        </div>
+        <button className=" flex justify-end item-center pt-8 pr-2">
+          <Button
+            variant="ghost"
+            onClick={() => redirectTo(`gallery/interview`)}
+          >
+            Ver todas as entrevistas <ArrowRightCircle className="w-6 h-6" />
+          </Button>
+        </button>
+      </div>
+    </>
+  )
+}
---
+++ src/app/about/terms-of-use/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import { Directory } from '@/app/components/Directory/Directory'
+
+export default function Terms() {
+  return (
+    <>
+      <div className="px-72">
+        <div className="pt-9 pb-52">
+          <Directory path1="Sobre" path2="Termos de uso" />
+        </div>
+        <div className="flex justify-center  flex-col pb-36">
+          <h1 className="text-title text-3xl font-bold pb-16 flex justify-center">
+            Termos de uso
+          </h1>
+          <div className="w-full flex justify-center text-justify">
+            <div className="text-brown w-[35rem] ">
+              Aceitação dos Termos: Ao acessar e utilizar o site Arte Concreta,
+              você concorda com os Termos de Uso e Política de Privacidade
+              abaixo detalhados. Se você não concordar com estes termos, não
+              deve utilizar este site. Direitos Autorais: Todo o conteúdo deste
+              site, incluindo, mas não se limitando a, textos, gráficos,
+              imagens, logotipos, são de propriedade exclusiva do site Arte
+              Concreta, e estão protegidos pela lei de direitos autorais. Você
+              não pode copiar, reproduzir, modificar, publicar ou distribuir
+              qualquer parte deste site sem a permissão prévia por escrito do
+              Arte Concreta. Conduta do Usuário: Você concorda em usar o site de
+              maneira responsável, não engajar em atividades que possam
+              danificar o site, e não usar o site para fins ilegais ou
+              inapropriados. Limitação de Responsabilidade: O site Arte Concreta
+              não se responsabiliza por qualquer dano resultante do uso ou
+              incapacidade de usar este site.
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center  flex-col pb-16">
+          <h1 className="text-title text-3xl font-bold pb-16 flex justify-center">
+            Política de Privacidade
+          </h1>
+          <div className="w-full flex justify-center text-justify">
+            <div className="text-brown w-[35rem] ">
+              Coleta de Informações: Coletamos informações pessoais, como nome e
+              endereço de e-mail, apenas quando você opta por fornecê-las, como
+              ao se inscrever para receber nossa newsletter. Coletamos
+              informações não pessoais, como dados de uso e cookies, para
+              melhorar a experiência do usuário. Uso de Informações: Usamos suas
+              informações para fornecer e melhorar nossos serviços, responder às
+              suas solicitações e comunicar com você. Não vendemos ou
+              compartilhamos suas informações pessoais com terceiros para fins
+              de marketing. Segurança das Informações: Tomamos medidas razoáveis
+              para proteger suas informações, mas não podemos garantir a
+              segurança total das informações que você fornece online. Cookies:
+              Usamos cookies para personalizar sua experiência no nosso site e
+              analisar o tráfego do site. Você pode configurar seu navegador
+              para recusar cookies, mas isso pode afetar a funcionalidade do
+              site. Alterações na Política: Podemos atualizar nossa política de
+              privacidade de tempos em tempos. Recomendamos que você revise esta
+              política regularmente para se manter informado sobre nossas
+              práticas de privacidade.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
