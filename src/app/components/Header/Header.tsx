fffc97671dbb7f6a302b525d16395b1217b1602b
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-10-12 18:41:11 -0300

    removendo as tags do footet

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -5,7 +5,10 @@ export default function Header() {
   return (
     <div className="flex  w-full items-center justify-between bg-base md:px-72 text-zinc-200  h-28 ">
       <Logo className="md:flex hidden" />
-      <HeaderTabs />
+      <div className="hidden">
+        {' '}
+        <HeaderTabs />
+      </div>
     </div>
   )
 }
