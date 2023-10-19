44692b1750331bcb5b6f926a3981e692ed845f2f
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-10-19 10:44:43 -0300

    colocando responsividade nos depoimentos

--- src/app/components/Testiominals/Testiominals.tsx
+++ src/app/components/Testiominals/Testiominals.tsx
@@ -18,22 +18,45 @@ const Testimonial = ({ testimonials }) => {
   }
 
   return (
-    <div className="w-full bg-zinc-200 py-11 px-72 flex items-center justify-between">
-      <button className="bg-arrow rounded-full p-3" onClick={prevTestimonial}>
+    <div className="w-full bg-zinc-200 py-11 lg:px-72 px-6 flex items-center flex-col lg:flex-row lg:justify-between">
+      <button
+        className="bg-arrow rounded-full p-3 hidden lg:block"
+        onClick={prevTestimonial}
+      >
         <CaretLeft className="h-5 w-5 text-white" />
       </button>
       <div className="flex flex-col px-96">
-        <div className="pb-4 bg-none text-Title text-7xl font-sans">´´</div>
-        <div className="text-sm text-brown pb-5 bg-none ">
+        <div className="pb-4 bg-none text-Title text-7xl font-sans hidden lg:block">
+          ´´
+        </div>
+        <div className="text-sm text-brown lg:pb-5 pb-16 bg-none text-justify w-80">
           {testimonials[currentIndex].depoiment}
         </div>
-        <div className="text-title text-xl">
+        <div className="text-title text-xl pb-10">
           {testimonials[currentIndex].author}
         </div>
       </div>
-      <button className="bg-arrow rounded-full p-3" onClick={nextTestimonial}>
+      <button
+        className="bg-arrow rounded-full p-3  hidden lg:block"
+        onClick={nextTestimonial}
+      >
         <CaretRight className="h-5 w-5 text-white" />
       </button>
+      <div className="flex gap-20  lg:hidden">
+        {' '}
+        <button
+          className="bg-arrow rounded-full p-3 "
+          onClick={prevTestimonial}
+        >
+          <CaretLeft className="h-5 w-5 text-white" />
+        </button>
+        <button
+          className="bg-arrow rounded-full p-3  lg:hidden"
+          onClick={nextTestimonial}
+        >
+          <CaretRight className="h-5 w-5 text-white" />
+        </button>
+      </div>
     </div>
   )
 }
